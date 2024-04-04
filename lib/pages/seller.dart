5325d927b7d4b67a06e78a16c855f4d9320ead9a
kushalram-k <kushalram.k2004@gmail.com>
2024-04-04 12:39:38 +0530

    updated seller page

--- lib/pages/seller.dart
+++ lib/pages/seller.dart
@@ -9,13 +9,13 @@ import 'package:liveauction/pages/seller_controller.dart';
 import 'package:liveauction/utils.dart';
 
 const List<String> list = [
-  'collectibles',
-  'antiques',
-  'art',
-  'electronics',
-  'books',
-  'sport',
-  'others',
+  'Collectibles',
+  'Antiques',
+  'Art & Craft',
+  'Electronics',
+  'Books',
+  'Sport',
+  'Others',
 ];
 
 class Seller extends StatefulWidget {
@@ -28,207 +28,195 @@ class Seller extends StatefulWidget {
 class _SellerState extends State<Seller> {
   String category = '';
   String dropdownValue = list.first;
-  
+
   // List<DropdownMenuEntry>;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
   // CalendarFormat _calendarFormat = CalendarFormat.month;
 
-
   DateTime? chosenDate;
-
-
-
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<Sellercontroller>(builder: (ctrl){
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Sell Item',
-          style: TextStyle(color: Color.fromARGB(255, 30, 28, 27)),
-        ),
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              const Text(
-                'Add Item Details',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 30.0),
-              TextFormField(
-                controller: ctrl.item_name,
-                decoration: const InputDecoration(
-                  labelText: 'Item name',
-                  focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 216, 97, 29))),
-                ),
-              ),
-              const SizedBox(height: 30.0),
-              TextFormField(
-                controller: ctrl.description,
-                minLines: 1,
-                maxLines: 10,
-                maxLength: 2000,
-                decoration: const InputDecoration(
-                  labelText: 'Item description',
-                  focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 216, 97, 29))),
-                ),
-              ),
-              TextFormField(
-                controller: ctrl.base_price,
-                keyboardType: TextInputType.number,
-                inputFormatters: <TextInputFormatter>[
-                  FilteringTextInputFormatter.digitsOnly
-                ],
-                decoration: const InputDecoration(
-                  labelText: 'base price',
-                  focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 216, 97, 29))),
-                ),
-              ),
-              const SizedBox(
-                height: 10.0,
-              ),
-              TextFormField(
-                controller: ctrl.locationchosen,
-                decoration: const InputDecoration(
-                  labelText: 'Location',
-                  focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 216, 97, 29))),
-                ),
-              ),
-               const SizedBox(
-                height: 10.0,
-              ),
-              TextFormField(
-                controller: ctrl.item_image,
-                decoration: const InputDecoration(
-                  labelText: 'Image Url',
-                  focusedBorder: OutlineInputBorder(
-                      borderSide:
-                          BorderSide(color: Color.fromARGB(255, 216, 97, 29))),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              if (chosenDate != null)
-              Text(
-                'Chosen Date: ${chosenDate!.day}/${chosenDate!.month}/${chosenDate!.year}',
-                style: TextStyle(fontSize: 16.0),
-              ),
-              SizedBox(height: 16.0),
-              ElevatedButton(
-              onPressed: () async {
-                // Show date picker and wait for user selection
-                final DateTime? pickedDate = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(2020),
-                  lastDate: DateTime(2100),
-                );
-
-                // Update chosenDate if the user selected a date
-                if (pickedDate != null) {
-                  setState(() {
-                    chosenDate = pickedDate;
-                    ctrl.datechosen=chosenDate;
-                    ctrl.checkSlotAvailability(chosenDate.toString().substring(0,10));
-                  });
-                }
-              },
-              child: Text('Choose Date'),
-            ),
-
-            SizedBox(height: 16.0),
-
-            // Show the chosen date if it's not null
-            
-              // Row(
-              //   children: [
-              //     ElevatedButton.icon(
-              //       onPressed: () => _showCalendar(),
-              //       icon: const Icon(Icons.calendar_today_rounded),
-              //       label: Text('Select Date: $_selectedDay'),
-              //     ),
-              //     const SizedBox(
-              //       width: 10.0,
-              //     ),
-              //     // Text(' $_selectedDay'),
-              //   ],
-              // ),
-              Center(
-                child: DropdownMenu<String>(
-                  inputDecorationTheme: InputDecorationTheme(
-                      fillColor: const Color.fromARGB(255, 216, 97, 29),
-                      filled: false,
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(1.0),
-                          borderSide: const BorderSide(
-                              color: Color.fromARGB(255, 216, 97, 29)))),
-                  dropdownMenuEntries:
-                      list.map<DropdownMenuEntry<String>>((String value) {
-                    return DropdownMenuEntry<String>(
-                        value: value, label: value);
-                  }).toList(),
-                  initialSelection: list.first,
-                  onSelected: (String? value) {
-                    
-                    // ctrl.selectedcategory=dropdownValue;
-                    // This is called when the user selects an item.
-                    setState(() {
-                      dropdownValue = value!;
-                    });
-                    ctrl.item_category=dropdownValue;
-                    ctrl.update();
-                  },
-                ),
-              ),
-             
-
-
-
-              const SizedBox(
-                height: 10.0,
-              ),
-              Center(
-                  child: ElevatedButton(
-                onPressed: () {
-                  ctrl.addproduct();
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color.fromARGB(255, 30, 28, 27),
-                ),
-                child: const Text(
-                  'Upload',
-                  style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
-                ),
-              ))
-            ],
+    return GetBuilder<Sellercontroller>(builder: (ctrl) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Sell Item',
+            style: TextStyle(color: Color.fromARGB(255, 30, 28, 27)),
           ),
         ),
-      ),
-    );
-  });
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'Add Item Details',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 30.0),
+                TextFormField(
+                  controller: ctrl.item_name,
+                  decoration: const InputDecoration(
+                    labelText: 'Item name',
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 216, 97, 29))),
+                  ),
+                ),
+                const SizedBox(height: 30.0),
+                TextFormField(
+                  controller: ctrl.description,
+                  minLines: 1,
+                  maxLines: 10,
+                  maxLength: 2000,
+                  decoration: const InputDecoration(
+                    labelText: 'Item description',
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 216, 97, 29))),
+                  ),
+                ),
+                TextFormField(
+                  controller: ctrl.base_price,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: <TextInputFormatter>[
+                    FilteringTextInputFormatter.digitsOnly
+                  ],
+                  decoration: const InputDecoration(
+                    labelText: 'base price',
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 216, 97, 29))),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                TextFormField(
+                  controller: ctrl.locationchosen,
+                  decoration: const InputDecoration(
+                    labelText: 'Location',
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 216, 97, 29))),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10.0,
+                ),
+                TextFormField(
+                  controller: ctrl.item_image,
+                  decoration: const InputDecoration(
+                    labelText: 'Image Url',
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                            color: Color.fromARGB(255, 216, 97, 29))),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                if (chosenDate != null)
+                  Text(
+                    'Chosen Date: ${chosenDate!.day}/${chosenDate!.month}/${chosenDate!.year}',
+                    style: TextStyle(fontSize: 16.0),
+                  ),
+                SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () async {
+                    // Show date picker and wait for user selection
+                    final DateTime? pickedDate = await showDatePicker(
+                      context: context,
+                      initialDate: DateTime.now(),
+                      firstDate: DateTime(2020),
+                      lastDate: DateTime(2100),
+                    );
+
+                    // Update chosenDate if the user selected a date
+                    if (pickedDate != null) {
+                      setState(() {
+                        chosenDate = pickedDate;
+                        ctrl.datechosen = chosenDate;
+                        ctrl.checkSlotAvailability(
+                            chosenDate.toString().substring(0, 10));
+                      });
+                    }
+                  },
+                  child: Text('Choose Date'),
+                ),
+
+                SizedBox(height: 16.0),
+
+                // Show the chosen date if it's not null
+
+                // Row(
+                //   children: [
+                //     ElevatedButton.icon(
+                //       onPressed: () => _showCalendar(),
+                //       icon: const Icon(Icons.calendar_today_rounded),
+                //       label: Text('Select Date: $_selectedDay'),
+                //     ),
+                //     const SizedBox(
+                //       width: 10.0,
+                //     ),
+                //     // Text(' $_selectedDay'),
+                //   ],
+                // ),
+                Center(
+                  child: DropdownMenu<String>(
+                    inputDecorationTheme: InputDecorationTheme(
+                        fillColor: const Color.fromARGB(255, 216, 97, 29),
+                        filled: false,
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(1.0),
+                            borderSide: const BorderSide(
+                                color: Color.fromARGB(255, 216, 97, 29)))),
+                    dropdownMenuEntries:
+                        list.map<DropdownMenuEntry<String>>((String value) {
+                      return DropdownMenuEntry<String>(
+                          value: value, label: value);
+                    }).toList(),
+                    initialSelection: list.first,
+                    onSelected: (String? value) {
+                      // ctrl.selectedcategory=dropdownValue;
+                      // This is called when the user selects an item.
+                      setState(() {
+                        dropdownValue = value!;
+                      });
+                      ctrl.item_category = dropdownValue;
+                      ctrl.update();
+                    },
+                  ),
+                ),
+
+                const SizedBox(
+                  height: 10.0,
+                ),
+                Center(
+                    child: ElevatedButton(
+                  onPressed: () {
+                    ctrl.addproduct();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 30, 28, 27),
+                  ),
+                  child: const Text(
+                    'Upload',
+                    style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),
+                  ),
+                ))
+              ],
+            ),
+          ),
+        ),
+      );
+    });
   }
-
-
-
-
-
 
   void _showCalendar() {
     showDialog(
